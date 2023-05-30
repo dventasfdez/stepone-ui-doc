d8e89ac2c17e31cfa6f36272054840de5e0acfe4
pablof89 <pablomf89@gmail.com>
2023-05-30 14:31:01 +0200

    remove redux thunk

--- src/redux/store/reducers.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-
-import notificationsReducer from "../slices/notificationsSlice";
-
-export const rootReducer = combineReducers({
-  notifications: notificationsReducer,
-});
