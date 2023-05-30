0c7d45f8317a25896cdb39ea43406bfaf275b38f
pablof89 <pablomf89@gmail.com>
2023-05-30 14:31:20 +0200

    remove redux thunk 2

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,29 +1,10 @@
 import notificationsReducer from "../slices/notificationsSlice";
+import { configureStore } from "@reduxjs/toolkit";
 
-import { configureStore, combineReducers, Action } from "@reduxjs/toolkit";
-import { ThunkAction } from "redux-thunk";
-
-export interface CombinedState {
-  notifications: any;
-  literals: any;
-}
-
-const rootReducer = combineReducers({
-  notifications: notificationsReducer,
+export const store = configureStore({
+  reducer: {
+    notifications: notificationsReducer,
+  },
 });
-
-export type RootState = ReturnType<typeof rootReducer>;
-
-const store = configureStore({
-  reducer: rootReducer,
-  devTools: process.env.NODE_ENV !== "production",
-  middleware: (getDefaultMiddleware: any) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-      immutableCheck: false,
-    }),
-});
-
-export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
 
 export default store;
