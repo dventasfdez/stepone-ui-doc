35d0cb1624a4180ca552f27f59d52c504b2ea160
pablof89 <pablomf89@gmail.com>
2023-06-07 20:08:04 +0200

    add result examples

--- src/app/documentation/data-table/page.tsx
+++ src/app/documentation/data-table/page.tsx
@@ -4,7 +4,7 @@ import React, { ReactElement } from "react";
 import Notification, { NotificationBody, NotificationHeader } from "stepone-ui/notification";
 import ComponentPropsTable from "@/components/content/documentantion/component-props-table";
 import ComponentExample from "@/components/content/documentantion/ComponentExample";
-import ResultsExample from "./results-examples";
+import ResultsExample, { result_props, results_example } from "./results-examples";
 import NoSSR from "@/components/shared/notifications/noSSR/noSSR";
 
 const ResultsGuideline: React.FC<any> = (): ReactElement => {
@@ -12,7 +12,8 @@ const ResultsGuideline: React.FC<any> = (): ReactElement => {
     <NoSSR>
       <h3>Results</h3>
 
-      <ResultsExample />
+      <ComponentExample codeExample={[{ name: "results_example.tsx", code: results_example }]} componentExample={<ResultsExample />} />
+      <ComponentPropsTable propsDefinition={result_props} title="Results Props" />
     </NoSSR>
   );
 };
